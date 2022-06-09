10fe37e841159c4aabc5a4e3b5d91d1441a1e88e
spidey143 <bekenev.daniilkpc582020@gmail.ru>
2022-06-09 23:19:28 +0300

    Функция номер 2

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ def way():
 
 
 def dictionary(path):
-    res = {}
-    for filename in os.listdir(path):
-        current_path = os.path.join(path, filename)
-        if os.path.isdir(current_path):
-            res.update(dictionary(current_path))
-        else:
-            res.update({current_path: os.path.getsize(current_path)})
-    return res
+  res = {}
+  for filename in os.listdir(path):
+      current_path = os.path.join(path, filename)
+      if os.path.isdir(current_path):
+          res.update(dictionary(current_path))
+      else:
+          res.update({current_path: os.path.getsize(current_path)})
+      return res
 
 
 def duplicate():
@@ -25,4 +25,4 @@ def my_print():
 
 
 if __name__ == '__main__':
-    pass
+  pass
